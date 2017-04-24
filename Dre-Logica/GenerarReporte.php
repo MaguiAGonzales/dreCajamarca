2cbf920946b9359f881910cdaa087390d6bb3ce8
Eidelman Hernandez <eidel_hs@hotmail.com>
2017-04-24 15:46:56 -0500

    avance reporte

--- Dre-Logica/GenerarReporte.php
+++ Dre-Logica/GenerarReporte.php
@@ -3,7 +3,133 @@
 	$id = $_GET['id'];
  
 	include('conexion.php');
-	require('../fpdf181/fpdf.php');
+	//require('../fpdf181/fpdf.php');
+	require('../dompdf/autoload.inc.php');
+
+	$query = "SELECT * FROM boleta where id_boleta = $id";
+
+	$consul = $conexion->query($query);
+
+	$datos = $consul->fetch_assoc();
+
+	$q_detalle  = "SELECT * FROM db_drecajamarca.detalle_bo d 
+	inner join otros o on d.id_otros = o.id_otros
+	 where id_boleta = $id";
+
+	$detalles = [];
+	$html_detalles = "";
+
+	$consul_det = $conexion->query($q_detalle);
+	while ($datos_det = $consul_det->fetch_assoc()) {
+		//$detalles[] = $datos_det;
+		$html_detalles .= "<tr><td>".$datos_det['nombre']."</td>"."<td>".$datos_det['dscto']."</td></tr>";
+	}
+
+
+	#generaremos el html de la tabla de detalles 
+
+
+
+
+	#generaremos ahora el HTML de too
+
+
+	$html = "<!DOCTYPE html>
+			<html lang='en'>
+			<head>
+				<meta charset='UTF-8'>
+				<title>Reporte</title>
+			</head>
+			<body>
+				
+				<!--los datos de la caabecera-->
+
+				<table>
+				<tr>
+					<td>imagen</td>
+					<td>Datos de otros</td>
+				</tr>
+					
+				</table>
+				
+				<hr>
+				<br>
+
+				<table>
+				<tr>
+					<td style='width: 240px;'>Descripcion </td>
+					<td  style='width: 140px;'>Monto</td>
+					<td style='width: 240px;'>Descripcion </td>
+					<td  style='width: 140px;'>Monto</td>
+				</tr>
+				<tr>
+					<td>Remuneracion Basica</td>
+					<td>".$datos['rem_bas']."</td>
+					<td></td>
+					<td></td>
+				</tr>
+					
+				</table>
+
+				<hr>
+				<br>
+
+				<!--Aca van los detalles -->
+				<table>
+				<tr>
+					<td>Descripcion</td>
+					<td>Monto</td>
+				</tr>
+				$html_detalles
+				
+				</table>
+
+				<hr>
+				<br>
+
+				<!--Aca van el total -->
+				<table>
+				<tr>
+					<td>Total: </td>
+					<td>".$datos['total']."  </td>
+				</tr>
+				
+				
+				</table>
+
+
+				
+			</body>
+			</html>";
+
+
+
+
+	// reference the Dompdf namespace
+	use Dompdf\Dompdf;
+
+	// instantiate and use the dompdf class
+	$dompdf = new Dompdf();
+	$dompdf->loadHtml($html);
+
+	// (Optional) Setup the paper size and orientation
+	$dompdf->setPaper('A4', 'landscape');
+
+	// Render the HTML as PDF
+	$dompdf->render();
+
+	// Output the generated PDF to Browser
+	$dompdf->stream();
+
+	
+
+
+
+
+
+	var_dump($detalles);
+
+	/*
 
 	class PDF extends FPDF
 	{
@@ -53,4 +179,8 @@
 			}
 
 		$pdf->Output();
+
+		*/
+
+
 ?>
--- Dre-Logica/conexion.php
+++ Dre-Logica/conexion.php
@@ -1,4 +1,4 @@
 <?php
-	$conexion = @mysqli_connect("localhost", "root", "", "db_drecajamarca") or die ("no se ha podido establecer conexion");
+	$conexion = @mysqli_connect("localhost", "root", "1234", "db_drecajamarca") or die ("no se ha podido establecer conexion");
 ?>
 
